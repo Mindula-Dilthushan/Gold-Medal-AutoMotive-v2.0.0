22514c4fc19d2f22eb787ded0babd1e7c0d44b12
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-13 19:53:39 +0530

    admin controller create

--- Backend/src/main/java/lk/controller/AdminController.java
+++ Backend/src/main/java/lk/controller/AdminController.java
@@ -1,7 +1,17 @@
 package lk.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/gma/v2/admin")
 public class AdminController {
+
+    @GetMapping
+    public String getAdmin(){
+        return "get Admin";
+    }
 }
