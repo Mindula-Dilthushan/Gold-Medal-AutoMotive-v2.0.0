5a66ec1e2f33ece519dd928fbe5ff2c769599a14
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-15 12:16:30 +0530

    admin repo create

---
+++ Backend/src/main/java/lk/repo/AdminRepo.java
@@ -0,0 +1,10 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.0
+package lk.repo;
+
+import lk.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepo extends JpaRepository<Admin,String> {
+}
