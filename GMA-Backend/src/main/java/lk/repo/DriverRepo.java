a025edbdf2a30a7f6c2017f0cf04ff58bc453372
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-16 17:58:49 +0530

    Driver Repo Create

---
+++ GMA-Backend/src/main/java/lk/repo/DriverRepo.java
@@ -0,0 +1,11 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-16
+package lk.repo;
+
+import lk.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DriverRepo extends JpaRepository<Driver,String> {
+}
