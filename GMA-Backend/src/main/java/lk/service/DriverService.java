ef3963fd1c84ec6f4a7aadc5cd9d9bd8b7368ecb
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-16 18:00:19 +0530

    DriverService create

---
+++ GMA-Backend/src/main/java/lk/service/DriverService.java
@@ -0,0 +1,16 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-16
+package lk.service;
+
+import lk.dto.DriverDTO;
+import java.util.ArrayList;
+
+public interface DriverService {
+    void saveDriver(DriverDTO driverDTO);
+    DriverDTO searchDriver(String id);
+    void updateDriver(DriverDTO driverDTO);
+    void deleteDriver(String id);
+    ArrayList<DriverDTO> getAllDrivers();
+}
