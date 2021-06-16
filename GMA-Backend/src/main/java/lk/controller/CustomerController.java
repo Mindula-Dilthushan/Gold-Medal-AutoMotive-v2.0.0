b3ff8fa8a43c5d1b00b6a38b1405356095e7e39e
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-16 14:58:01 +0530

    customerController save success

---
+++ GMA-Backend/src/main/java/lk/controller/CustomerController.java
@@ -0,0 +1,33 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-16
+package lk.controller;
+
+import lk.dto.CustomerDTO;
+import lk.exeption.NotFoundException;
+import lk.service.CustomerService;
+import lk.util.StandardResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/v2/customer")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity saveCustomer(@RequestBody CustomerDTO customerDTO){
+        if (customerDTO.getCustomerId().trim().length() <=0){
+            throw new NotFoundException("Customer id cannot be empty");
+        }
+        customerService.saveCustomer(customerDTO);
+        return new ResponseEntity(new StandardResponse("201","Done",customerDTO), HttpStatus.CREATED);
+    }
+}
