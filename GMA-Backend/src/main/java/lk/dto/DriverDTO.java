5d5a7af70d89354d248e2cae2339f98471ac80bd
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-16 17:57:44 +0530

    Driver DTO create

--- GMA-Backend/src/main/java/lk/dto/DriverDTO.java
+++ GMA-Backend/src/main/java/lk/dto/DriverDTO.java
@@ -4,5 +4,19 @@
 //21-06-16
 package lk.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class DriverDTO {
+
+    private String DriverId;
+    private String DriverName;
+    private String DriverContact;
+    private String DriverNIC;
 }
