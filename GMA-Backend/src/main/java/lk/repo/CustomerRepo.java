bc54bdf41a65361f229a61d33e432b0cce1fe6e0
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-16 14:20:04 +0530

    customer repo added

---
+++ GMA-Backend/src/main/java/lk/repo/CustomerRepo.java
@@ -0,0 +1,11 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-16
+package lk.repo;
+
+import lk.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<Customer,String> {
+}
