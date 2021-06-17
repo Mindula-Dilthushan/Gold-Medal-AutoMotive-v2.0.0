620d3f7979b75e68703a4b71462b5a66dd1d4cc3
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-17 11:24:03 +0530

    carMaintenance added

---
+++ GMA-Backend/src/main/java/lk/entity/CarMainTenance.java
@@ -0,0 +1,28 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-17
+package lk.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CarMainTenance {
+    @Id
+    private String mainTenanceId;
+    private Date mainTenanceDate;
+    private String mainTenanceDescription;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "carid",referencedColumnName = "carId",nullable = false)
+    private Car car;
+}
