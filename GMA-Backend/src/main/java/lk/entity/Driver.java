7cb0acc9830c984443589bc495f0c4430fa4fb6d
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-17 11:40:14 +0530

    Driver Entity complete

--- GMA-Backend/src/main/java/lk/entity/Driver.java
+++ GMA-Backend/src/main/java/lk/entity/Driver.java
@@ -4,13 +4,17 @@
 //21-06-16
 package lk.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -24,5 +28,9 @@ public class Driver {
     private String DriverName;
     private String DriverContact;
     private String DriverNIC;
+
+    @OneToMany(mappedBy = "driver",cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Booking> bookingList = new ArrayList<>();
 
 }
