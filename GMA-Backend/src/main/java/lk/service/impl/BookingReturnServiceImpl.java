9e03e1bcca4cbef2178b1ac6a9728c241b91ffd0
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-17 12:11:14 +0530

    bookingReturnServiceImpl Create

---
+++ GMA-Backend/src/main/java/lk/service/impl/BookingReturnServiceImpl.java
@@ -0,0 +1,37 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-17
+package lk.service.impl;
+
+import lk.dto.BookingDTO;
+import lk.service.BookingService;
+import java.util.ArrayList;
+
+public class BookingReturnServiceImpl implements BookingService {
+
+    @Override
+    public void saveBooking(BookingDTO bookingDTO) {
+
+    }
+
+    @Override
+    public void updateBooking(BookingDTO bookingDTO) {
+
+    }
+
+    @Override
+    public void deleteBooking(String id) {
+
+    }
+
+    @Override
+    public BookingDTO searchBooking(String id) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<BookingDTO> getAllBooking() {
+        return null;
+    }
+}
