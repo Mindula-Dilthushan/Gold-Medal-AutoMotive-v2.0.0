c6cc7c4a43b18dffd0862d3987402e53199d7a6c
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-17 12:24:01 +0530

    bookingReturs save complete

--- GMA-Backend/src/main/java/lk/service/impl/BookingReturnServiceImpl.java
+++ GMA-Backend/src/main/java/lk/service/impl/BookingReturnServiceImpl.java
@@ -4,34 +4,58 @@
 //21-06-17
 package lk.service.impl;
 
-import lk.dto.BookingDTO;
-import lk.service.BookingService;
-import java.util.ArrayList;
+import lk.dto.BookingReturnDTO;
+import lk.entity.BookingReturn;
+import lk.exeption.ValidateException;
+import lk.repo.BookingRepo;
+import lk.repo.BookingReturnRepo;
+import lk.service.BookingReturnService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.util.List;
 
-public class BookingReturnServiceImpl implements BookingService {
+@Service
+@Transactional
+public class BookingReturnServiceImpl implements BookingReturnService {
+
+
+    @Autowired
+    private BookingReturnRepo bookingReturnRepo;
+
+    @Autowired
+    private BookingRepo bookingRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
-    public void saveBooking(BookingDTO bookingDTO) {
+    public void saveBookingReturn(BookingReturnDTO bookingReturnDTO) {
+        if (bookingReturnRepo.existsById(bookingReturnDTO.getBookingReturnId())) {
+            throw new ValidateException("BookingReturn Already Exist");
+        }
+        System.out.println(bookingReturnDTO);
+        bookingReturnRepo.save(modelMapper.map(bookingReturnDTO, BookingReturn.class));
+    }
+
+    @Override
+    public void updateBookingReturn(BookingReturnDTO bookingReturnDTO) {
 
     }
 
     @Override
-    public void updateBooking(BookingDTO bookingDTO) {
+    public void deleteBookingReturn(String id) {
 
     }
 
     @Override
-    public void deleteBooking(String id) {
-
-    }
-
-    @Override
-    public BookingDTO searchBooking(String id) {
+    public BookingReturnDTO searchBookingReturn(String id) {
         return null;
     }
 
     @Override
-    public ArrayList<BookingDTO> getAllBooking() {
+    public List<BookingReturnDTO> getAllBookingReturns() {
         return null;
     }
 }
