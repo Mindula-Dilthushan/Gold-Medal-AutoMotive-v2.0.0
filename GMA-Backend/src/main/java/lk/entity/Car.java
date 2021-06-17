0ea46df255ae8252b363159469e62cb53968513d
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-17 11:33:52 +0530

    car entity complete

--- GMA-Backend/src/main/java/lk/entity/Car.java
+++ GMA-Backend/src/main/java/lk/entity/Car.java
@@ -9,7 +9,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
