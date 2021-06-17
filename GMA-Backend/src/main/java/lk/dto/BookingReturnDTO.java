2d8d4da1d40d5c9d17410cb6f62383b7d18b3e73
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-17 11:42:48 +0530

    booking return dto create

---
+++ GMA-Backend/src/main/java/lk/dto/BookingReturnDTO.java
@@ -0,0 +1,18 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-17
+package lk.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+public class BookingReturnDTO {
+
+}
