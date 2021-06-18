dfb5bb534b508bc615d2fbdc2792939bebf05bc9
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-18 14:44:43 +0530

    admin dashboard modify

--- GMA-Frontend/src/js/Admin.js
+++ GMA-Frontend/src/js/Admin.js
@@ -1,42 +1,41 @@
-let btnArray = ['#btn_mainPage', '#btn_ProfilePage','btn_CustomerPage'];
-
-function setClass() {
-    for (id in btnArray) {
-        $(btnArray[id]).removeClass(
-            'btn-custom-selected').
-        addClass('btn-custom');
-    }
-}
-
-let hide = ['#Main_Page', '#Profile_Page', '#Customers_Page'];
-
-function hideAll() {
-    for (let i in hide) {
-        $(hide[i]).hide();
-    }
-
-}
+// $('#btn_mainPage').on('click', function () {
+//     $('#admin_Home_Page').css({
+//         'display': 'block'
+//     });
+//     $('#admin_Customer_Page').css({
+//         'display': 'none'
+//     });
+//     $('#admin_profile_Page').css({
+//         'display': 'none'
+//     });
+// });
+//
+// $('#btn_ProfilePage').on('click', function () {
+//     $('#admin_Home_Page').css({
+//         'display': 'none'
+//     });
+//     $('#admin_Customer_Page').css({
+//         'display': 'none'
+//     });
+//     $('#admin_profile_Page').css({
+//         'display': 'block'
+//     });
+// });
+//
+// $('#btn_CustomerPage').on('click', function () {
+//     $('#admin_Home_Page').css({
+//         'display': 'none'
+//     });
+//     $('#admin_Customer_Page').css({
+//         'display': 'block'
+//     });
+//     $('#admin_profile_Page').css({
+//         'display': 'none'
+//     });
+// });
 
 $('#btn_mainPage').click(() => {
-    hideAll();
-    setClass();
     $('#btn_mainPage').addClass('btn-custom-selected');
-    $('#Main_Page').fadeIn(1000);
-
-});
-
-$('#btn_ProfilePage').click(() => {
-    hideAll();
-    setClass();
-    $('#btn_ProfilePage').addClass('btn-custom-selected');
-    $('#Profile_Page').fadeIn(1000);
-
-});
-
-$('#btn_CustomerPage').click(() => {
-    hideAll();
-    setClass();
-    $('#btn_CustomerPage').addClass('btn-custom-selected');
-    $('#Customers_Page').fadeIn(1000);
+    $('#admin_Home_Page').fadeIn(1000);
 
 });
