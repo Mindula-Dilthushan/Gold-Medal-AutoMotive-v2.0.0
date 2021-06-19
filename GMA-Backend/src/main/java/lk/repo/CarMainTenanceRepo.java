5cdc46f723bd85503e0204e74497cb238603ea0a
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-19 11:07:36 +0530

    carMainTenance Repo added

---
+++ GMA-Backend/src/main/java/lk/repo/CarMainTenanceRepo.java
@@ -0,0 +1,11 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-19
+package lk.repo;
+
+import lk.entity.CarMainTenance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarMainTenanceRepo extends JpaRepository<CarMainTenance,String>{
+}
