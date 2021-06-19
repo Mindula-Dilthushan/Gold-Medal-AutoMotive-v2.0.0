f8efd4f90033791345789840e2433a1a3552ef67
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-19 10:56:46 +0530

    payment service added

---
+++ GMA-Backend/src/main/java/lk/service/PaymentService.java
@@ -0,0 +1,16 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-19
+package lk.service;
+
+import lk.dto.PaymentDTO;
+import java.util.ArrayList;
+
+public interface PaymentService {
+    void savePayment(PaymentDTO paymentDTO);
+    void deletePayment(PaymentDTO paymentDTO);
+    void updatePayment(PaymentDTO paymentDTO);
+    PaymentDTO searchPayment(String id);
+    ArrayList<PaymentDTO> getAllPayments();
+}
---
+++ GMA-Backend/src/main/java/lk/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,41 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-19
+package lk.service.impl;
+
+import lk.dto.PaymentDTO;
+import lk.service.PaymentService;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+
+@Service
+@Transactional
+public class PaymentServiceImpl implements PaymentService {
+
+    @Override
+    public void savePayment(PaymentDTO paymentDTO) {
+
+    }
+
+    @Override
+    public void deletePayment(PaymentDTO paymentDTO) {
+
+    }
+
+    @Override
+    public void updatePayment(PaymentDTO paymentDTO) {
+
+    }
+
+    @Override
+    public PaymentDTO searchPayment(String id) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<PaymentDTO> getAllPayments() {
+        return null;
+    }
+}
