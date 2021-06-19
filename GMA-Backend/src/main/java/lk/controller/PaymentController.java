eb3231fa54d7b2835f9ce97632c9d178af52296d
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-19 11:26:38 +0530

    payment controller added

---
+++ GMA-Backend/src/main/java/lk/controller/PaymentController.java
@@ -0,0 +1,15 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-19
+package lk.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/v2/payment")
+public class PaymentController {
+}
