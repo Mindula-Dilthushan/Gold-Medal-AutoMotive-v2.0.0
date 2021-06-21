45b8b1b8d64e32282e2c73ff23492979f66a66d7
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-21 17:30:27 +0530

    Login Entity complete

---
+++ GMA-Backend/src/main/java/lk/entity/Login.java
@@ -0,0 +1,25 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-17
+package lk.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Login {
+    @Id
+    private String loginEmail;
+    private String loginFName;
+    private String loginLName;
+    private String loginPassword;
+}
