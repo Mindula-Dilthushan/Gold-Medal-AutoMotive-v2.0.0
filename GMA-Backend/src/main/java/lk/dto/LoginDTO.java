65f7dae61493f99b70918f56b96a714fc0285018
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-21 17:38:17 +0530

    login dto complete

--- GMA-Backend/src/main/java/lk/dto/LoginDTO.java
+++ GMA-Backend/src/main/java/lk/dto/LoginDTO.java
@@ -4,5 +4,19 @@
 //21-06-21
 package lk.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class LoginDTO {
+
+    private String loginEmail;
+    private String loginFName;
+    private String loginLName;
+    private String loginPassword;
 }
