b60e6f4ec7b16320007ed43a3f81aac7df571b63
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-21 17:43:18 +0530

    loginServiceImpl class create

---
+++ GMA-Backend/src/main/java/lk/service/impl/LoginServiceImpl.java
@@ -0,0 +1,37 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-21
+package lk.service.impl;
+
+import lk.dto.LoginDTO;
+import lk.service.LoginService;
+import java.util.ArrayList;
+
+public class LoginServiceImpl implements LoginService {
+    
+    @Override
+    public void saveLogin(LoginDTO loginDTO) {
+
+    }
+
+    @Override
+    public void deleteLogin(String id) {
+
+    }
+
+    @Override
+    public void updateLogin(LoginDTO loginDTO) {
+
+    }
+
+    @Override
+    public LoginDTO searchLogin(String id) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<LoginDTO> getAllLogs() {
+        return null;
+    }
+}
