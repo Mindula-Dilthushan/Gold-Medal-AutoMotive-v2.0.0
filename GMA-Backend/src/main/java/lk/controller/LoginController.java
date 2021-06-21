d8a8e6a51bf9c3951e20cfcdf9ed8a9b92e4ee47
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-21 18:06:20 +0530

    Login Controller SaveLogin complete

---
+++ GMA-Backend/src/main/java/lk/controller/LoginController.java
@@ -0,0 +1,33 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-21
+package lk.controller;
+
+import lk.dto.LoginDTO;
+import lk.exeption.NotFoundException;
+import lk.service.LoginService;
+import lk.util.StandardResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/v2/login")
+public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity saveLogin(@RequestBody LoginDTO loginDTO){
+        if (loginDTO.getLoginEmail().trim().length() <=0){
+            throw new NotFoundException("Login id cannot be empty");
+        }
+        loginService.saveLogin(loginDTO);
+        return new ResponseEntity(new StandardResponse("201","Done",loginDTO), HttpStatus.CREATED);
+    }
+}
---
+++ GMA-Frontend/src/js/login/Register.js
@@ -0,0 +1,70 @@
+//Start Login Validation Section
+function checkValidationRegister() {
+
+    let loginFName = $('#txtFName').val();
+    let loginLName = $('#txtLName').val();
+    let loginEmail = $('#txtEmail').val();
+    let loginPassword = $('#txtPassword').val();
+    let loginConfirmPassword = $('#txtConfirmPassword').val();
+
+    if (loginFName != "") {
+        if (loginLName != "") {
+            if (loginEmail != "") {
+                if (loginPassword != "") {
+                    if (loginConfirmPassword) {
+                        return true;
+                    } else {
+                        alert("Please Enter Confirm Password");
+                        return false;
+                    }
+                } else {
+                    alert("Please Enter Password");
+                    return false;
+                }
+            } else {
+                alert("Please Enter Email");
+                return false;
+            }
+        } else {
+            alert("Please Enter Last Name");
+            return false;
+        }
+    } else {
+        alert("Please Enter First Name");
+        return false;
+    }
+}
+//End Login Validation Section
+
+//Start Admin Save Section
+$('#btn_Register').click(() => {
+
+    if (checkValidationRegister()) {
+        let loginFName = $('#txtFName').val();
+        let loginLName = $('#txtLName').val();
+        let loginEmail = $('#txtEmail').val();
+        let loginPassword = $('#txtPassword').val();
+        let loginConfirmPassword = $('#txtConfirmPassword').val();
+
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:8080/GMA/v2/login",
+            data: JSON.stringify({
+                "customerId": loginFName,
+                "customerName": loginLName,
+                "customerAddress": loginEmail,
+                "customerEmail": loginPassword,
+                "customerNIC": loginConfirmPassword
+            }),
+            dataType: 'Json',
+            contentType: "application/json; charset=utf-8",
+            success: function (res) {
+                if (res.message == 'Success') {
+                }
+            },
+            error: function (ob, textStatus, error) {
+            }
+        });
+    }
+});
+//End Admin Save Section
