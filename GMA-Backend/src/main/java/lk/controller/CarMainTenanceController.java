b183d11fed555fb4cf78ddf0801649bab88caba0
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-21 16:59:20 +0530

    car mentenance controller class create

---
+++ GMA-Backend/src/main/java/lk/controller/CarMainTenanceController.java
@@ -0,0 +1,16 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-21
+package lk.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/v2/maintenance")
+public class CarMainTenanceController {
+
+}
