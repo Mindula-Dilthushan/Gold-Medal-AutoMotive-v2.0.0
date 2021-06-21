03ee4306fbf339b9d4e375abc4553a0db2b3ec3c
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-21 17:39:23 +0530

    LoginRepo complete

---
+++ GMA-Backend/src/main/java/lk/repo/LoginRepo.java
@@ -0,0 +1,11 @@
+//Alpha X Software Company
+//Mindula Dilthushan
+//GMA v2.0.3
+//21-06-21
+package lk.repo;
+
+import lk.entity.Login;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoginRepo extends JpaRepository<Login,String> {
+}
