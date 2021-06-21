f91d12870db58cfc0b35b86d6233c231602c9b2b
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-21 17:10:57 +0530

    car mentenance controller crud complete

--- GMA-Backend/src/main/java/lk/controller/CarMainTenanceController.java
+++ GMA-Backend/src/main/java/lk/controller/CarMainTenanceController.java
@@ -13,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 
 @RestController
