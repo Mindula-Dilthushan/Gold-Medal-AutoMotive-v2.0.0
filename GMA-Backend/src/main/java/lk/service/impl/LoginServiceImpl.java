01ddafd69021a1876812c0112a7f257097c520bd
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-21 17:44:42 +0530

    saveLogin complete

--- GMA-Backend/src/main/java/lk/service/impl/LoginServiceImpl.java
+++ GMA-Backend/src/main/java/lk/service/impl/LoginServiceImpl.java
@@ -5,14 +5,35 @@
 package lk.service.impl;
 
 import lk.dto.LoginDTO;
+import lk.entity.Login;
+import lk.exeption.ValidateException;
+import lk.repo.LoginRepo;
 import lk.service.LoginService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 
+@Service
+@Transactional
 public class LoginServiceImpl implements LoginService {
-    
+
+    @Autowired
+    private LoginRepo loginRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
     public void saveLogin(LoginDTO loginDTO) {
-
+        if (loginRepo.existsById(loginDTO.getLoginEmail())){
+            throw new ValidateException("Login Already Exist");
+        }
+        System.out.println(loginDTO);
+        loginRepo.save(
+                modelMapper.map(loginDTO, Login.class)
+        );
     }
 
     @Override
