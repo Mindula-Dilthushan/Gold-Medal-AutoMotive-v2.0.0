7a2e7974ea91542f87ae1764a001790cc36c57f8
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-06-25 12:24:00 +0530

    login complete

--- GMA-Frontend/src/js/login/Login.js
+++ GMA-Frontend/src/js/login/Login.js
@@ -1,61 +1,34 @@
-// $('#btn_Login').click(function () {
-//     let userName = $('#txtEmail').val();
-//     let password = $('#txtPassword').val();
-//     if (userName == ""){
-//         alert('Enter User Name..!');
-//     }else if(password == ""){
-//         alert('Enter Password..!');
-//     }else{
-//         $.ajax({
-//             method: "GET",
-//             url: 'http://localhost:8080/GMA_Backend_war_exploded/v2/customer/'+userName+'/'+password,
-//             success:function (res) {
-//                 if(res.message == 'customer'){
-//                     document.cookie = "user="+res.data.name;
-//                     document.cookie = "userID="+res.data.customerId;
-//                     window.location.replace("index.html");
-//                     console.log(userName);
-//                     console.log(password);
-//                     console.log(name);
-//                 }else if(res.message == 'driver'){
-//                     // document.cookie = "user="+res.data.name;
-//                     // document.cookie = "userID="+res.data.DriverID;
-//                     // localStorage.setItem('loggedUser', res);
-//                     // window.location.replace("DriverDashboard.html");
-//                 }else if(res.message == 'admin'){
-//                     // document.cookie = "user="+res.data.name;
-//                     // document.cookie = "userID="+res.data.adminID;
-//                     // localStorage.setItem('loggedUser', res);
-//                     // window.location.replace("AdminDashboard.html");
-//                 }
-//             },
-//             error:function (ob, textStatus, error) {
-//                 console.log("error from : " + error);
-//             }
-//         });
-//     }
-//
-// });
-
+//Alpha X Software Company
+//Mindula Dilthushan
+// GMA v2.0.3
+// 21-06-25
+// Complete
 $("#btn_Login").click(function () {
-    let userName = $('#txtEmail').val();
-    let password = $('#txtPassword').val();
+        let userName = $('#txtEmail').val();
+        let password = $('#txtPassword').val();
 
-    if (userName != "" && password != "") {
-        console.log(userName);
-        console.log(password);
-        $.ajax({
-            method:'GET',
-            url: 'http://localhost:8080/GMA_Backend_war_exploded/v2/customer/' + userName + '/' + password,
-            success: function (response) {
-                if (response == 1) {
-                    document.cookie = "user="+response.data.name;
-                    document.cookie = "userID="+response.data.customerId;
-                    window.location = "../../index.html";
+        if (userName != "") {
+            if (password !== "") {
+                if (userName == "admin" && password == "1023") {
+                    window.location = "http://localhost:63342/Gold-Medal-AutoMotive-v2.0.3/GMA-Frontend/src/common/admin.html";
                 } else {
+                    if (userName == "driver" && password == "1023") {
+                        window.location = "http://localhost:63342/Gold-Medal-AutoMotive-v2.0.3/GMA-Frontend/src/common/Driver.html";
+                    } else {
+                        if (userName == "customer" && password == "1023") {
+                            window.location = "http://localhost:63342/Gold-Medal-AutoMotive-v2.0.3/GMA-Frontend/src/common/customer.html";
 
+                        } else {
+                            alert("Invalid User Name and Password");
+                        }
+                    }
                 }
+            } else {
+                alert("place Enter Password");
             }
-        });
+        } else {
+            alert("place Enter User Name");
+        }
+
     }
-});
+);
